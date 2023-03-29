5d64f8e3f379e2c2ca4cdc7ab5adbd9dbd39ad3c
Daniel Nguyen <mduongbkhn@gmail.com>
2023-03-29 21:11:47 +0900

    add functional

---
+++ agents/ddpg/modules/backbone.py
@@ -0,0 +1,63 @@
+# Pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class Actor(nn.Module):
+    def __init__(
+            self,
+            in_dim: int,
+            out_dim: int,
+            init_w: float = 3e-3,
+    ):
+        """Initialize."""
+        super(Actor, self).__init__()
+        self.hidden1 = nn.Linear(in_dim, 128)
+        self.hidden2 = nn.Linear(128, 128)
+        self.out = nn.Linear(128, out_dim)
+        self.out.weight.data.uniform_(-init_w, init_w)
+        self.out.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        x = F.relu(self.hidden1(state))
+        x = F.tanh(self.hidden2(x))
+        x = F.relu(self.hidden2(x))
+        x = F.tanh(self.hidden2(x))
+        x = F.relu(self.hidden2(x))
+        action = self.out(x).tanh()
+
+        return action
+
+
+class Critic(nn.Module):
+    def __init__(
+            self,
+            in_dim: int,
+            init_w: float = 3e-3,
+    ):
+        """Initialize."""
+        super(Critic, self).__init__()
+
+        self.hidden1 = nn.Linear(in_dim, 128)
+        self.hidden2 = nn.Linear(128, 128)
+        self.out = nn.Linear(128, 1)
+
+        self.out.weight.data.uniform_(-init_w, init_w)
+        self.out.bias.data.uniform_(-init_w, init_w)
+
+    def forward(
+            self, state: torch.Tensor, action: torch.Tensor
+    ) -> torch.Tensor:
+        """Forward method implementation."""
+        x = torch.cat((state, action), dim=-1)
+        x = F.relu(self.hidden1(x))
+        x = F.tanh(self.hidden2(x))
+        x = F.relu(self.hidden2(x))
+        x = F.tanh(self.hidden2(x))
+        x = F.relu(self.hidden2(x))
+        value = self.out(x)
+
+        return value
---
+++ agents/ddpg/modules/utils.py
@@ -0,0 +1,110 @@
+import numpy as np
+import random
+import copy
+
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process.
+    Taken from Udacity deep-reinforcement-learning github repository:
+    https://github.com/udacity/deep-reinforcement-learning/blob/master/
+    ddpg-pendulum/ddpg_agent.py
+    """
+
+    def __init__(
+        self,
+        size: int,
+        mu: float = 0.0,
+        theta: float = 0.15,
+        sigma: float = 0.2,
+    ):
+        """Initialize parameters and noise process."""
+        self.state = np.float64(0.0)
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self) -> np.ndarray:
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array(
+            [random.random() for _ in range(len(x))]
+        )
+        self.state = x + dx
+        return self.state
+
+
+import os
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+USE_CUDA = torch.cuda.is_available()
+FLOAT = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
+
+def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
+def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
+def prYellow(prt): print("\033[93m {}\033[00m" .format(prt))
+def prLightPurple(prt): print("\033[94m {}\033[00m" .format(prt))
+def prPurple(prt): print("\033[95m {}\033[00m" .format(prt))
+def prCyan(prt): print("\033[96m {}\033[00m" .format(prt))
+def prLightGray(prt): print("\033[97m {}\033[00m" .format(prt))
+def prBlack(prt): print("\033[98m {}\033[00m" .format(prt))
+
+def to_numpy(var):
+    return var.cpu().data.numpy() if USE_CUDA else var.data.numpy()
+    # return var.cpu().data.detach().numpy() if USE_CUDA else var.data.numpy()
+
+def to_tensor(ndarray, volatile=False, requires_grad=False, dtype=FLOAT):
+  return Variable(torch.from_numpy(ndarray), volatile=volatile, requires_grad=requires_grad).type(dtype)
+    # return Variable(torch.from_numpy(ndarray), volatile=volatile, requires_grad=requires_grad).type(dtype = np.float16)
+    # return Variable(torch.from_numpy(int64), volatile=volatile, requires_grad=requires_grad).type(dtype)
+
+
+def soft_update(target, source,o):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(
+            target_param.data * (1.0 - o) + param.data * o
+        )
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
+
+def get_output_folder(parent_dir, env_name):
+    """Return save folder.
+    Assumes folders in the parent_dir have suffix -run{run
+    number}. Finds the highest run number and sets the output folder
+    to that number + 1. This is just convenient so that if you run the
+    same script multiple times tensorboard can plot all of the results
+    on the same plots with different names.
+    Parameters
+    ----------
+    parent_dir: str
+      Path of the directory containing all experiment runs.
+    Returns
+    -------
+    parent_dir/run_dir
+      Path to this run's save directory.
+    """
+    os.makedirs(parent_dir, exist_ok=True)
+    experiment_id = 0
+    for folder_name in os.listdir(parent_dir):
+        if not os.path.isdir(os.path.join(parent_dir, folder_name)):
+            continue
+        try:
+            folder_name = int(folder_name.split('-run')[-1])
+            if folder_name > experiment_id:
+                experiment_id = folder_name
+        except:
+            pass
+    experiment_id += 1
+
+    parent_dir = os.path.join(parent_dir, env_name)
+    parent_dir = parent_dir + '-run{}'.format(experiment_id)
+    os.makedirs(parent_dir, exist_ok=True)
+    return parent_dir
