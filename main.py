0314d7fea33754161114c66c9a8001d2611dd7ab
Daniel Nguyen <mduongbkhn@gmail.com>
2023-03-29 14:42:00 +0900

    add .py

--- main.py
+++ main.py
@@ -11,6 +11,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pass
