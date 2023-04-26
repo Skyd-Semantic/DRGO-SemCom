181f08315325d23bd65055f532e1701cf2235819
Daniel Nguyen <mduongbkhn@gmail.com>
2023-04-26 12:33:29 +0900

    Update env_agent_utils.py
    Simplify environment.py

---
+++ envs/env_agent_utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+from utils.setting_setup import *
+import scipy
+
+
+class env_agent_utils():
+    def __init__(self):
+        pass
+
+    def _wrapState(self):
+        self.ChannelGain = self._ChannelGain_Calculated()
+        state = np.concatenate((np.array(self.ChannelGain).reshape(1, -1), np.array(self.U_location).reshape(1, -1),
+                                np.array(self.User_trajectory).reshape(1, -1)), axis=1)
+        return state
+
+    def _decomposeState(self, state):
+        H = state[0: self.N_User]
+        U_location = state[self.N_User: 2 * self.N_User + 2]
+        User_trajectory = state[self.N_User + 2: 2 * self.N_User + 4]
+        return [
+            np.array(H), np.array(U_location), np.array(User_trajectory)
+        ]
+
+    def _wrapAction(self):
+        action = np.concatenate((np.array([[self.tau]]).reshape(1, self.N_User),
+                                 np.array([[self.o]]).reshape(1, self.N_User),
+                                 np.array([[self.P_n]]).reshape(1, self.N_User)), axis=1)
+
+        return action
+
+    def _decomposeAction(self, action):
+        # make output for resource allocation tau (range: [0,P_u_max])
+        # make output for power (range: [0,1])
+        # make output for compression ratio: (range: [0,1])
+        tau = action[0][0: self.N_User].astype(float)
+        tau = scipy.special.softmax(tau, axis=None)
+        print(f"tau: {tau}")
+        o = action[0][self.N_User: 2 * self.N_User].astype(float)
+        P_n = (action[0][2 * self.N_User: 3 * self.N_User].astype(float))*self.P_u_max
+
+        # print(f"tau: {tau}")
+        # print(f"o: {o}")
+        # print(f"P_n: {P_n}")
+
+        return [
+            np.array(tau).reshape((1,self.N_User)),
+            np.array(o).reshape((1,self.N_User)),
+            np.array(P_n).reshape((1,self.N_User))
+        ]
+
+        # return [
+        #         np.array(tau).reshape(1, self.N_User).squeeze(),
+        #         np.array(o).reshape(1, self.N_User).squeeze(),
+        #         np.array(P_n).reshape(1, self.N_User).squeeze()
+        #        ]
