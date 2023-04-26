765f97f6a0b74639f5c4b73ed2cce5ea5f74943f
Daniel Nguyen <mduongbkhn@gmail.com>
2023-04-26 15:28:10 +0900

    Update environment.py

--- envs/environment.py
+++ envs/environment.py
@@ -33,15 +33,24 @@ class DRGO_env(env_utils, env_agent_utils):
         self.BS_R_Range = 1
         self.BS_R_min = 0.1
 
+        # Goal-oriented Settings
+        self.acc_threshold = 0.05
+        self.Lipschitz = 0.005
+
+        self.lamda = 0.1
+
+        self.sigma_data = 0.01
+        self.semantic_mode = args.semantic_mode
+
         """ =============== """
         """     Actions     """
         """ =============== """
         self.o = np.random.randint(0, self.N_User, size=[self.N_User,1])
-        # tau is Sub-carrier-Allocation. It is an array with form of Num_Nodes interger number, value change from [0:Num_sub-1] (0 means Sub#1)
+        # tau is Sub-carrier-Allocation. It is an array with form of Num_Nodes interger number,
+        # value change from [0:Num_sub-1] (0 means Sub#1)
         self.tau = np.random.randint(0, self.N_User, size=[self.N_User,1])
-        print(f"{np.shape(self.tau)}-{np.shape(self.tau)}")
-        # self.beta = np.reshape(np.random.randint(0, self.N_User, size = self.N_User), self.N_User)
-        # eta is AP-Allocation. It is an array with form of Num_Nodes interger number, value change from [0:Num_APs-1] (0 means Sub#1
+        # eta is AP-Allocation. It is an array with form of Num_Nodes interger number,
+        # value change from [0:Num_APs-1] (0 means Sub#1)
         self.P_n = np.reshape((np.random.rand(1, self.N_User) * self.P_u_max), (self.N_User,1))
 
         """ ========================================= """
@@ -50,10 +59,9 @@ class DRGO_env(env_utils, env_agent_utils):
         self.BS_location = np.expand_dims(self._location_BS_Generator(), axis=0)
         self.U_location = self._location_CU_Generator()
         self.User_trajectory = self._trajectory_U_Generator()
-        # self.User_trajectory = self._trajectory_U_Generator()
         self.distance_CU_BS = self._distance_Calculated(self.U_location, self.BS_location)
 
-        self.ChannelGain = self._ChannelGain_Calculated()
+        self.ChannelGain = self._ChannelGain_Calculated(self.sigma_data)
         self.commonDataRate = self._calculateDataRate(self.ChannelGain)
         self.T = 0                                           # initialize rewards)
 
@@ -70,17 +78,30 @@ class DRGO_env(env_utils, env_agent_utils):
         # Environment change
         self.User_trajectory = np.expand_dims(self._trajectory_U_Generator(), axis=0)
         self.U_location = self.User_trajectory + self.U_location
-        # state wrap
+        # State wrap
         state_next = self._wrapState()
-        # re-calculate channel gain
-        self.ChannelGain = self._ChannelGain_Calculated()
+        # Re-calculate channel gain
+        self.ChannelGain = self._ChannelGain_Calculated(self.sigma_data)
 
         self.T = self._Time()    # Generate self.T
-        # print(reward)
-        reward = self.T
 
-        # reward = T - alpha_1 * constraint_1 - ... - alpha_n * constraint_n
-        # T ~ 0.1s -> alpha * constraint < 0.01
+        # Calculate distortion rate
+        sigma_data = self.sigma_data
+
+        # We assume that it follows CLT
+        # This function can be changed in the future
+        temp_c = 20
+        sigma_sem = np.exp(temp_c*(1-self.o)**2)
+        sigma_tot_sqr = 1/((1/sigma_sem**2)+(1/sigma_data**2))
+
+        # Goal-oriented penalty
+        if self.semantic_mode == "learn":
+            penalty = (self.eta**2 * self.Lipschitz/2 - self.eta)*\
+                      (self.Lipschitz**2) * sigma_tot_sqr - self.acc_threshold
+        else:
+            penalty = (self.eta ** 2 * self.Lipschitz / 2 - self.eta)*\
+                      (self.Lipschitz**2) * sigma_tot_sqr - self.acc_threshold
+        reward = self.T - self.lamda*penalty
         """
         T = 100 
         - Normally, the constraint * penalty should be around 0.01 - 0.2 of T
@@ -104,7 +125,7 @@ class DRGO_env(env_utils, env_agent_utils):
         self.distance_CU_BS = self._distance_Calculated(self.BS_location, self.U_location)
 
         # re-calculate channel gain
-        self.ChannelGain = self._ChannelGain_Calculated()
+        self.ChannelGain = self._ChannelGain_Calculated(self.sigma_data)
 
         # Generate next state [set of ChannelGain]
         state_next = self._wrapState()
