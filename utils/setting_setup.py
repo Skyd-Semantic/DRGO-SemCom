c8feab75b45fd9635ff2c95ca019a7f6999a749a
Daniel Nguyen <mduongbkhn@gmail.com>
2023-04-29 00:18:28 +0900

    Update setting_setup.py

--- utils/setting_setup.py
+++ utils/setting_setup.py
@@ -79,7 +79,7 @@ def get_arguments():
                         help="max number of step per episode")
     parser.add_argument("--semantic-mode", type=str, default="learn",
                         help="learn | infer")
-    parser.add_argument("--pen-coeff", type=float, default=1,
+    parser.add_argument("--pen-coeff", type=float, default=0,
                         help="coefficient for penalty")
 
     return parser.parse_args()
