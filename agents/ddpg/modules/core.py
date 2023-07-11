feae26ce47a5ce256f36f04093fae640ade9fe6e
Daniel Nguyen <mduongbkhn@gmail.com>
2023-07-11 14:22:40 +0900

    Update core.py

--- agents/ddpg/modules/core.py
+++ agents/ddpg/modules/core.py
@@ -64,3 +64,72 @@ class Critic(nn.Module):
         value = self.out(x)
 
         return value
+
+
+"""
+    Resnet-9 for Actor/Critic
+"""
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += residual
+        out = F.relu(out)
+        return out
+
+
+class Actor_Resnet9(nn.Module):
+    def __init__(self, in_dim, out_dim, init_w=3e-3):
+        super(Actor, self).__init__()
+        self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.residual_blocks = nn.Sequential(
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64)
+        )
+        self.out = nn.Linear(64, out_dim)
+        self.out.weight.data.uniform_(-init_w, init_w)
+        self.out.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state):
+        x = F.relu(self.bn1(self.conv1(state)))
+        x = self.residual_blocks(x)
+        x = F.avg_pool2d(x, kernel_size=x.size()[2:])
+        x = x.view(x.size(0), -1)
+        action = self.out(x).sigmoid()
+        return action
+
+
+class Critic_Resnet9(nn.Module):
+    def __init__(self, in_dim, init_w=3e-3):
+        super(Critic, self).__init__()
+        self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.residual_blocks = nn.Sequential(
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64)
+        )
+        self.out = nn.Linear(64, 1)
+        self.out.weight.data.uniform_(-init_w, init_w)
+        self.out.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state, action):
+        x = F.relu(self.bn1(self.conv1(state)))
+        x = self.residual_blocks(x)
+        x = F.avg_pool2d(x, kernel_size=x.size()[2:])
+        x = x.view(x.size(0), -1)
+        x = torch.cat((x, action), dim=-1)
+        value = self.out(x)
+        return value
