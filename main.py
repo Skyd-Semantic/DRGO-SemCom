c68068df3cdabea83d2164bc429e68ee1fd3bbe9
Daniel Nguyen <mduongbkhn@gmail.com>
2023-08-04 12:45:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from agents.ddpg.agent import *
 from utils.setting_setup import *
 from envs.environment import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = get_arguments()
