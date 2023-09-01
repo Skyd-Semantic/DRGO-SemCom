733c2328e2add8854b68e7f3489865f52362d5d1
Daniel Nguyen <mduongbkhn@gmail.com>
2023-09-02 01:11:01 +0900

    Update commcal_utils.py

--- envs/commcal_utils.py
+++ envs/commcal_utils.py
@@ -1,15 +1,21 @@
-
 import sys
 import numpy as np
 from math import log10
 
+
 def mW2dBm(mW):
     return 10. * log10(mW)
+
 
 # Function to convert from dBm to mW
 def dBm2mW(dBm):
     return 10 ** ((dBm) / 10.)
 
+
 def dBm2W(W):
     return 10 ** (W / 10) / 1000
 
+
+def W2dBm(W):
+    # Convert the power vector from watts to dBm
+    return 10 * np.log10(W * 1000)
